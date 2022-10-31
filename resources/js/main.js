912d9e8cb4f75d0bd4474ff2879587959d17d070
Yasir Younas <yazyounas@gmail.com>
2022-10-31 13:25:23 +0000

    added javascript in contact page

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,4 +1,41 @@
 
+let navButtons = document.getElementsByClassName("nav-btn");
+
+for (var i=0; i < navButtons.length; i++) { 
+    // On mouse over.
+    navButtons[i].addEventListener("mouseover", function () {
+        // Change color to red.
+        
+        this.style.backgroundColor = 'gray';
+        this.style.transform = 'scale(1.2)';
+        
+        
+    });
+    // On mouse out.
+    navButtons[i].addEventListener("mouseout", function () {
+      // Change back to original color.
+      this.style.backgroundColor = '';
+      
+      this.style.transform = '';
+    });
+}
+
+let Buttons = document.getElementsByClassName("btn");
+
+for (let i=0; i < navButtons.length; i++) { 
+    // On mouse over.
+    Buttons[i].addEventListener("mouseover", function () {
+        // Change color to red.
+        this.style.color = "black";
+    });
+    // On mouse out.
+    Buttons[i].addEventListener("mouseout", function () {
+      // Change back to original color.
+      this.style.color = "";
+    });
+}
+
+
 /*
 nav.forEach((li) =>{
  
@@ -8,8 +45,10 @@ nav.forEach((li) =>{
 
 
 */
+/*
+let changebg = document.querySelector('.nav-btn');
 
- /*   
+
 function changeBackground() {
     changebg.style.backgroundColor = 'gray';
     changebg.style.fontSize= '24px';
@@ -26,7 +65,7 @@ function changeBackground() {
   changebg.addEventListener('mouseout', normalBackground);
 */
   // The keys and notes variables store the piano keys
- 
+ /*
 const unorderList = ['list1', 'list2', 'list3', 'list4','list5','list6','list7','list8'];
 const notes = [];
 unorderList.forEach(function(key){
@@ -35,14 +74,14 @@ unorderList.forEach(function(key){
 
 // Write named functions that change the color of the keys below
 const keyPlay = function(event) {
-  event.target.style.backgroundColor = 'grey'
-  event.target.style.color = 'black'
+  event.target.style.backgroundColor = 'red'
+  
   event.target.style.fontSize = '24px'
  
 }
 const keyReturn = function(event) {
   event.target.style.backgroundColor = '';
-  event.target.style.color = '';
+  
   event.target.style.fontSize = '';
 }
 // Write a named function with event handler properties
@@ -55,4 +94,4 @@ let changeColor = function(note) {
 }
 }
 // Write a loop that runs the array elements through the function
-notes.forEach(changeColor);
+notes.forEach(changeColor);*/
